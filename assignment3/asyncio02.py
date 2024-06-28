dbd77d49671b1303f83bf6fdedf14fd1963a47fd
Pon <pon.m22@gmail.com>
2024-06-28 11:14:46 +0700

    Done Assignment3

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,13 @@
 # example of running a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
 
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+
+# create tbe coroutine
+asyncio.run(main())
