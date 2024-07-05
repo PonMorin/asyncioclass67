ee88ed569ccea9c7e0795a904c270f79a7e1d11b
Pon <pon.m22@gmail.com>
2024-07-05 11:28:21 +0700

    :memo: Done breakfast03.py

--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -12,16 +12,24 @@ async def make_coffee():       # 1
 async def fry_eggs():          # 1
     print("eggs: prepare ingridients")
     sleep(1)
-    print("eggs: frying...")    # 2: pause, another tasks can be run
-    await asyncio.sleep(3)
+    print("eggs: frying...")    
+    await asyncio.sleep(3)     # 2: pause, another tasks can be run
     print("eggs: ready")
+
+async def toast_break():       # 1
+    print("toastL prepare ingridients")
+    sleep(1)
+    print("toast: break")
+    await asyncio.sleep(3)    # 2: pause, another tasks can be run
 
 async def main():                     # 1
     start = time()
-    coffee_task = asyncio.create_task(make_coffee())
-    egg_task = asyncio.create_task(fry_eggs())
-    await coffee_task
+    coffee_task = asyncio.create_task(make_coffee())        # Create Task for coffee
+    egg_task = asyncio.create_task(fry_eggs())              # Create Task for egg
+    toast_task = asyncio.create_task(toast_break())         # Create Task for toast
+    await coffee_task  
     await egg_task
+    await toast_task
     print(f"breakfast is ready in {time()-start} min")
 
 
