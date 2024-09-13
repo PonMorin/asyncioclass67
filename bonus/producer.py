56bd6e02cf05be9ef808b2537b2fd8f6a2640172
Pon <pon.m22@gmail.com>
2024-09-13 11:52:18 +0700

    :memo: Done Bonus

--- bonus/producer.py
+++ bonus/producer.py
@@ -1,16 +1,15 @@
 # https://aiokafka.readthedocs.io/en/stable/
 from aiokafka import AIOKafkaProducer
-import random
 import asyncio
 
 async def send_one():
     producer = AIOKafkaProducer(
-        bootstrap_servers='172.16.46.25:9094')
+        bootstrap_servers='localhost:9092')
     # Get cluster layout and initial topic/partition leadership information
     await producer.start()
     try:
         # Produce message
-            await producer.send_and_wait("my_topic", b"Pon message")
+        await producer.send_and_wait("my_topic", b"Super message")
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
